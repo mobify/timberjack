37b872199b8cb208b39c0e98a3890f427c0ab3e0
Sebastian Vetter <seb@roadsi.de>
2016-09-20 14:45:39 -0700

    Add configuration module

---
+++ timberjack/configure.py
@@ -0,0 +1,14 @@
+import structlog
+
+from .processors import CensorSensitiveDataProcessor
+
+
+def configure_structlog():
+    structlog.configure(
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        processors=[
+            structlog.processors.JSONRenderer(),
+            # Must be the last processor in the list
+            CensorSensitiveDataProcessor(),
+        ],
+    )
